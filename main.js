dc1eb6a148bdbe225a5c671cf6e7116727b29b14
Sandip Narbat <Sandipnarbat111@gmail.com>
2023-03-22 01:43:43 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,14 @@ let inputValue = document.getElementById("search");
 const searchBtn = document.querySelector("#searchBtn");
 var url = 'https://www.google.com/search?q=';
 
+
+inputValue.addEventListener("keypress", function(event) {
+    if (event.key==="Enter") {
+        url = "https://www.google.com/search?q=" + inputValue.value;
+        window.open(url, '_self');
+    }
+})
+
 searchBtn.addEventListener("click",()=>{
     if(inputValue.value===""){
         console.log("nothing to search")
